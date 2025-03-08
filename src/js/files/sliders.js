65615b4c7617a2e6f4bf667b906c6b063be710e6
Oleg Bashylov <olegbash94@gmail.com>
2025-03-08 23:56:33 +0200

    design-adv_fullDONE

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -30,80 +30,146 @@ function initSliders() {
 	// Перевіряємо, чи є слайдер на сторінці
 	if (document.querySelector('.swiper')) { // Вказуємо склас потрібного слайдера
 		// Створюємо слайдер
-		new Swiper('.design-adv__slider', { // Вказуємо склас потрібного слайдера
-			// Підключаємо модулі слайдера
-			// для конкретного випадку
-			modules: [Navigation],
-			observer: true,
-			observeParents: true,
-			slidesPerView: 1,
-			spaceBetween: 0,
-			//autoHeight: true,
-			speed: 800,
+		// new Swiper('.design-adv__slider', { // Вказуємо склас потрібного слайдера
+		// 	// Підключаємо модулі слайдера
+		// 	// для конкретного випадку
+		// 	modules: [Navigation],
+		// 	observer: true,
+		// 	observeParents: true,
+		// 	slidesPerView: 3.4,
+		// 	spaceBetween: 57,
+		// 	//autoHeight: true,
+		// 	speed: 800,
 
-			//touchRatio: 0,
-			//simulateTouch: false,
-			//loop: true,
-			//preloadImages: false,
-			//lazy: true,
+		// 	//touchRatio: 0,
+		// 	//simulateTouch: false,
+		// 	//loop: true,
+		// 	//preloadImages: false,
+		// 	//lazy: true,
 
-			/*
-			// Ефекти
-			effect: 'fade',
-			autoplay: {
-				delay: 3000,
-				disableOnInteraction: false,
-			},
-			*/
+		// 	/*
+		// 	// Ефекти
+		// 	effect: 'fade',
+		// 	autoplay: {
+		// 		delay: 3000,
+		// 		disableOnInteraction: false,
+		// 	},
+		// 	*/
 
-			// Пагінація
-			/*
-			pagination: {
-				el: '.swiper-pagination',
-				clickable: true,
-			},
-			*/
+		// 	// Пагінація
+		// 	/*
+		// 	pagination: {
+		// 		el: '.swiper-pagination',
+		// 		clickable: true,
+		// 	},
+		// 	*/
 
-			// Скроллбар
-			/*
-			scrollbar: {
-				el: '.swiper-scrollbar',
-				draggable: true,
-			},
-			*/
+		// 	// Скроллбар
+		// 	/*
+		// 	scrollbar: {
+		// 		el: '.swiper-scrollbar',
+		// 		draggable: true,
+		// 	},
+		// 	*/
 
-			// Кнопки "вліво/вправо"
-			navigation: {
-				prevEl: '.swiper-button-prev',
-				nextEl: '.swiper-button-next',
-			},
-			/*
-			// Брейкпоінти
-			breakpoints: {
-				640: {
-					slidesPerView: 1,
-					spaceBetween: 0,
-					autoHeight: true,
-				},
-				768: {
-					slidesPerView: 2,
-					spaceBetween: 20,
-				},
-				992: {
-					slidesPerView: 3,
-					spaceBetween: 20,
-				},
-				1268: {
-					slidesPerView: 4,
-					spaceBetween: 30,
-				},
-			},
-			*/
-			// Події
-			on: {
+		// 	// Кнопки "вліво/вправо"
+		// 	navigation: {
+		// 		prevEl: '.swiper-button-prev',
+		// 		nextEl: '.swiper-button-next',
+		// 	},
+		// 	/*
+		// 	// Брейкпоінти
+		// 	breakpoints: {
+		// 		640: {
+		// 			slidesPerView: 1,
+		// 			spaceBetween: 0,
+		// 			autoHeight: true,
+		// 		},
+		// 		768: {
+		// 			slidesPerView: 2,
+		// 			spaceBetween: 20,
+		// 		},
+		// 		992: {
+		// 			slidesPerView: 3,
+		// 			spaceBetween: 20,
+		// 		},
+		// 		1268: {
+		// 			slidesPerView: 4,
+		// 			spaceBetween: 30,
+		// 		},
+		// 	},
+		// 	*/
+		// 	// Події
+		// 	on: {
 
+		// 	}
+		// });
+
+
+
+		const resizableSwiper = (breakpoint, swiperClass, swiperSettings, callback) => {
+			let swiper;
+
+			breakpoint = window.matchMedia(breakpoint);
+
+			const enableSwiper = function (className, settings) {
+				swiper = new Swiper(className, settings);
+
+				if (callback) {
+					callback(swiper);
+				}
 			}
-		});
+
+			const checker = function () {
+				if (breakpoint.matches) {
+					return enableSwiper(swiperClass, swiperSettings);
+				} else {
+					if (swiper !== undefined) swiper.destroy(true, true);
+					return;
+				}
+			};
+
+			breakpoint.addEventListener('change', checker);
+			checker();
+		}
+
+		const someFunc = (instance) => {
+			if (instance) {
+				instance.on('slideChange', function (e) {
+					console.log('*** mySwiper.activeIndex', instance.activeIndex);
+				});
+			}
+		};
+
+		resizableSwiper(
+			'(min-width: 767.98px)',
+			'.design-adv__slider',
+			{
+				loop: true,
+				spaceBetween: 57,
+				slidesPerView: 3.37,
+				// pagination: {
+				// 	el: '.swiper-pagination',
+				// 	clickable: true,
+				// },
+				// Брейкпоінти
+				breakpoints: {
+					768: {
+						slidesPerView: 2.5,
+						spaceBetween: 40,
+					},
+					992: {
+						slidesPerView: 3.37,
+						spaceBetween: 57,
+					},
+
+				},
+			},
+			someFunc
+		);
+
+
+
 	}
 }
 // Скролл на базі слайдера (за класом swiper scroll для оболонки слайдера)
